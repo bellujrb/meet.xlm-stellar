eba53590a0ec5c677dc4eabed30a0859e3edb1bb
bellujrb <98968823+bellujrb@users.noreply.github.com>
2025-11-22 18:19:43 -0300

    fix: pwa

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg', 'pwa-icon.svg'],
       manifest: {
         name: 'Meet.XLM - Stellar Events',
         short_name: 'Meet.XLM',
